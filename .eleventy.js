e3e90cb5826d3d493e5dfd2a5f82d611d362da0b
Frederik De Bleser <frederik@debleser.be>
2021-06-11 14:53:24 +0200

    Make example of custom collection.

--- .eleventy.js
+++ .eleventy.js
@@ -2,4 +2,10 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("admin/config.yml");
+
+  eleventyConfig.addCollection("digitalContext", (collectionApi) => {
+    return collectionApi
+      .getAll()
+      .filter((item) => item.data.context === "Digital Context");
+  });
 };
