d3f78579e0ea63207f3cef2e1688d476c31d2646
Frederik De Bleser <frederik@debleser.be>
2021-06-11 14:54:12 +0200

    Add fonts passthrough folder.

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("fonts");
   eleventyConfig.addPassthroughCopy("admin/config.yml");
 
   eleventyConfig.addCollection("digitalContext", (collectionApi) => {
