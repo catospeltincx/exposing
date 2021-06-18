eb947cfd286d56d8d7eb31ca3ccdecc4cd6874a3
catospeltincx <cato.speltincx@telenet.be>
2021-06-18 17:54:02 +0200

    add collections for each context

--- .eleventy.js
+++ .eleventy.js
@@ -9,4 +9,22 @@ module.exports = function (eleventyConfig) {
       .getAll()
       .filter((item) => item.data.context === "Digital Context");
   });
+
+  eleventyConfig.addCollection("autonomousContext", (collectionApi) => {
+    return collectionApi
+      .getAll()
+      .filter((item) => item.data.context === "Autonomous Context");
+  });
+
+  eleventyConfig.addCollection("appliedContext", (collectionApi) => {
+    return collectionApi
+      .getAll()
+      .filter((item) => item.data.context === "Applied Context");
+  });
+
+  eleventyConfig.addCollection("sociopoliticalContext", (collectionApi) => {
+    return collectionApi
+      .getAll()
+      .filter((item) => item.data.context === "Socio-Political Context");
+  });
 };
